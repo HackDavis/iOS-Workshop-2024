c0a708565f0e068438c4e01e2a22a3148d744063
brandonw504 <brandonw504@outlook.com>
2024-02-19 16:50:21 -0800

    Initialized Xcode project

---
+++ iOS-Workshop-2024/iOS_Workshop_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Workshop_2024App.swift
+//  iOS-Workshop-2024
+//
+//  Created by Brandon Wong on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Workshop_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
